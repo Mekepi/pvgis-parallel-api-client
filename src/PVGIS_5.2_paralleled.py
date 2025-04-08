6acd93c7f8a7a3097cb7d37a68b551b417b46f22
Raphael Rodrigues <128844797+Mekepi@users.noreply.github.com>
2025-04-07 21:22:51 -0300

    Update PVGIS_5.2_paralleled.py
    v8

--- src/PVGIS_5.2_paralleled.py
+++ src/PVGIS_5.2_paralleled.py
@@ -1,11 +1,14 @@
 import requests
 import multiprocessing as m
+import numpy as np
 import time
 
 def request_hourlydata(line):
     [lat,lon,startyear,endyear] = line.split(",")
     [lat,lon,startyear,endyear] = [float(lat),float(lon),int(startyear),int(endyear)]
+    request_start = time.monotonic()
     response = requests.get("https://re.jrc.ec.europa.eu/api/v5_2/seriescalc?lat=%f&lon=%f&startyear=%i&endyear=%i" %(lat,lon,startyear,endyear))
+    print("Request duration:", time.monotonic()-request_start)
     try:
         f = open("hourlydata(%f,%f)[%i,%i].csv"%(lat,lon,startyear,endyear), "x")
     except FileExistsError as err:
@@ -27,9 +30,34 @@ def pvgis_5_2_hourlydata(file):
             print(err)
 
         else:
+            v_process = np.vectorize(lambda line: m.Process(target=request_hourlydata, args=[line]), [type(m.Process())])
+            coords = inputs.readlines()
+            processes = np.array_split(np.array(v_process(np.array([coords])))[0], np.ceil(len(coords)/m.cpu_count()))
+            #print(processes)
 
-            m.Pool(m.cpu_count()).map_async(request_hourlydata, inputs.readlines()).wait()
+            total_request_delay = 0
+            joining_duration = 0
+            chunk_rest = m.cpu_count()/30
+            for processes_chunk in processes:
+                sleep_duration = time.time()
+                np.vectorize(lambda process: process.start(),otypes=[type(None)])(processes_chunk)
+                """ joining_start =  time.time()
+                np.vectorize(lambda process: process.join(),otypes=[type(None)])(processes_chunk)
+                joining_duration += time.time()-joining_start """
+                sleep_duration = chunk_rest-(time.time()-sleep_duration)
+                if (sleep_duration>0): 
+                    time.sleep(sleep_duration)
+                    total_request_delay += sleep_duration
+            print("  Sleep duration:", total_request_delay)
 
-        print("execution time: %f" %(time.time()-start_time))
+            joining_duration =  time.time()
+            for process_chunk in processes:
+                for process in process_chunk:
+                    process.join()
+            joining_duration = time.time()-joining_duration
+            print("Joining duration:", joining_duration)
+            total_request_delay += joining_duration
+
+        print("total request delay: %f\nexecution time: %f" %(total_request_delay, time.time()-start_time))
             
 pvgis_5_2_hourlydata("inputs2.dat")
