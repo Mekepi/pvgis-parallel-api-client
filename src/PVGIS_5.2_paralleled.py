9945839876b2c261f8555146c0add6c0a9a2695b
Raphael Rodrigues <128844797+Mekepi@users.noreply.github.com>
2025-04-07 21:23:47 -0300

    Update PVGIS_5.2_paralleled.py
    v12

--- src/PVGIS_5.2_paralleled.py
+++ src/PVGIS_5.2_paralleled.py
@@ -2,53 +2,69 @@ from urllib3 import request
 from multiprocessing import Process, Pipe
 from multiprocessing.connection import PipeConnection
 from time import time, sleep
-from  os import remove
-from os.path import dirname, abspath
+from os import remove, mkdir
+from os.path import dirname, abspath, isdir, isfile, getsize
+from pathlib import Path
+from psutil import virtual_memory
 
-def request_hourlydata(coord:str, con:PipeConnection) -> None:
+
+
+def request_timeseries(coord:str, con:PipeConnection) -> None:
+
     line:list = [float(coord.split(",")[0]),float(coord.split(",")[1]),int(coord.split(",")[2]),int(coord.split(",")[3])]
-    
-    file:str = "%s\\data\\hourlydata(%.6f,%.6f)[%i,%i].csv"%(dirname(abspath(__file__)),line[0],line[1],line[2],line[3])
+    file_path:Path = Path("%s\\data\\timeseries(%.6f,%.6f)[%i,%i].csv"%(dirname(abspath(__file__)),line[0],line[1],line[2],line[3]))
+
     try:
-        f = open(file, "xb")
-    except FileExistsError as err:
-        print(err)
+        file = open(file_path, "xb")
+    except FileExistsError:
+        if (getsize(file_path)>0):
+            None#print(err)
+        else:
+            remove(file_path)
+            con.send(coord)
     except Exception as err:
         print(err)
         con.send(coord)
     else: 
         try:
-            f.write(request("GET","https://re.jrc.ec.europa.eu/api/v5_2/seriescalc?lat=%.6f&lon=%.6f&startyear=%i&endyear=%i" %(line[0],line[1],line[2],line[3]),preload_content=False,timeout=None).data)
+            file.write(request("GET","https://re.jrc.ec.europa.eu/api/v5_2/seriescalc?lat=%.6f&lon=%.6f&startyear=%i&endyear=%i&components=1" %(line[0],line[1],line[2],line[3]),preload_content=False,retries=False,timeout=None).data)
         except Exception as err:
-            f.close()
-            remove(file)
+            file.close()
+            remove(file_path)
             print(err)
             con.send(coord)
         else:
-            f.close()
-            with open(file, "r") as f:
+            file.close()
+            with open(file_path, "r") as f:
                 b:bool = f.readline().startswith("Latitude (decimal degrees):")
             if (not(b)):
-                remove(file)
+                remove(file_path)
                 con.send(coord)
     con.close()
 
 
-def main(file:str) -> None:
-    start_time = time()
+def main(file_path:Path) -> None:
+    start_time:float = time()
 
-    with open("%s\\%s"%(dirname(abspath(__file__)), file), "r") as inputs:
+    with open(file_path, "r") as inputs:
         lines:list[str] = inputs.readlines()
 
-    parent_cons:map[PipeConnection]
-    child_cons:map[PipeConnection]
+    parent_cons:tuple[PipeConnection]
+    child_cons:tuple[PipeConnection]
     parent_cons, child_cons = zip(*map(lambda _: Pipe(False), range(len(lines))))
-    processes:list[Process] = list(map(lambda line, con: Process(target=request_hourlydata, args=[line,con]), lines, child_cons))
+    processes:list[Process] = list(map(lambda line, con: Process(target=request_timeseries, args=[line,con]), lines, child_cons))
     
+    if (not(isdir("%s\\data"%(dirname(abspath(__file__)))))):
+        mkdir("%s\\data"%(dirname(abspath(__file__))))
+    
+    sleep_count:int = 0
     for process in processes:
         process.start()
-        sleep(0.04)
-    print("  Sleep duration:", 0.04*len(processes))
+        sleep(0.1)
+        while ((virtual_memory()[0]-virtual_memory()[3])/(1024**2)<311):
+            sleep(1)
+            sleep_count += 1
+    print("  Sleep duration:", 0.1*len(processes)+sleep_count)
 
     for process in processes:
         process.join()
@@ -57,12 +73,15 @@ def main(file:str) -> None:
     
     parent_recvs:list[str] = [con.recv() for con in parent_cons if con.poll()]
     if (parent_recvs):
-        with open("%s\\retry.dat"%(dirname(abspath(__file__))), "w") as retry:
+        retry_path:Path = Path("%s\\retry.dat"%(dirname(abspath(__file__))))
+        with open(retry_path, "w") as retry:
             retry.writelines(parent_recvs)
         print("Retrying %i coordinates..."%(len(parent_recvs)))
-        main("retry.dat")
-
+        main(retry_path)
+        if (isfile(retry_path)):
+            remove(retry_path)
+    
     print("execution time: %f" %(time()-start_time))
             
 if __name__ == '__main__':
-    main("inputs2.dat")
+    main(Path("SP.dat"))
