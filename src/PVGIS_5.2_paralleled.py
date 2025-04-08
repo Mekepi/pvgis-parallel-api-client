31a14e45362b89d707081d186388679688ee2c75
Raphael Rodrigues <128844797+Mekepi@users.noreply.github.com>
2025-04-07 21:22:19 -0300

    Update PVGIS_5.2_paralleled.py
    v6

--- src/PVGIS_5.2_paralleled.py
+++ src/PVGIS_5.2_paralleled.py
@@ -1,21 +1,24 @@
-import requests
+import urllib3 as u
 import multiprocessing as m
+import numpy as np
 import time
 
-def request_hourlydata(line:str):
+def request_hourlydata(line):
     line = line.split(",")
     line = [float(line[0]),float(line[1]),int(line[2]),int(line[3])]
     try:
-        f = open("hourlydata(%f,%f)[%i,%i].csv"%(line[0],line[1],line[2],line[3]), "x+b")
+        f = open("hourlydata(%f,%f)[%i,%i].csv"%(line[0],line[1],line[2],line[3]), "x")
     except FileExistsError as err:
         print(err)
     else:
         request_start = time.monotonic()
-        f.write(requests.get("https://re.jrc.ec.europa.eu/api/v5_2/seriescalc?lat=%f&lon=%f&startyear=%i&endyear=%i" %(line[0],line[1],line[2],line[3])).content)
+        f.write(u.request("GET","https://re.jrc.ec.europa.eu/api/v5_2/seriescalc?lat=%f&lon=%f&startyear=%i&endyear=%i" %(line[0],line[1],line[2],line[3]),preload_content=False,timeout=None).data)
         print("Request", m.current_process().name.rsplit("-")[1] ,"duration:", time.monotonic()-request_start)
         f.close()
 
 def pvgis_5_2_hourlydata(file):
+    
+    if __name__ == '__main__':
 
         start_time = time.time()
 
@@ -26,18 +29,27 @@ def pvgis_5_2_hourlydata(file):
             print(err)
 
         else:
-            processes = list(m.Process(target=request_hourlydata, args=[line]) for line in inputs.readlines())
-            inputs.close()
-
-            total_request_delay = 0.04*len(processes)
-            for process in processes:
-                process.start()
-                time.sleep(0.04)
+            c_process = np.vectorize(lambda line: m.Process(target=request_hourlydata, args=[line]))
+            processes = np.array(c_process(np.array([inputs.readlines()])))
             
+            i=0; j=0
+            v_start = np.vectorize(lambda process: process.start(),otypes=[type(None)], cache=True)
+            chunk_minimun_rest = (m.cpu_count()/30)*1.01
+            total_request_delay = 0
+            for i in range(m.cpu_count(), np.shape(processes)[1]+1, m.cpu_count()):
+                v_start(processes[0,j:i])
+                j=i
+                time.sleep(1)
+                total_request_delay += chunk_minimun_rest
+            if (i<np.shape(processes)[1]):
+                j=i
+                i=np.shape(processes)[1]
+                v_start(processes[0,j:i])
+
             print("  Sleep duration:", total_request_delay)
             print("   Until joining:", time.time()-start_time)
             joining_start = time.time()
-            for process in processes:
+            for process in processes[0]:
                 process.join()
                 process.close()
             print("Joining duration:", time.time()-joining_start)
@@ -45,5 +57,4 @@ def pvgis_5_2_hourlydata(file):
 
         print("total request delay: %f\nexecution time: %f" %(total_request_delay, time.time()-start_time))
             
-if __name__ == '__main__':
-    pvgis_5_2_hourlydata("inputs2.dat")
+pvgis_5_2_hourlydata("inputs2.dat")
