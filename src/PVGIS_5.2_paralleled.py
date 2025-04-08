576ae472468f55aa0baae71724a3d654643ebf18
Raphael Rodrigues <128844797+Mekepi@users.noreply.github.com>
2025-04-07 21:23:32 -0300

    Update PVGIS_5.2_paralleled.py
    v11

--- src/PVGIS_5.2_paralleled.py
+++ src/PVGIS_5.2_paralleled.py
@@ -1,65 +1,68 @@
 from urllib3 import request
 from multiprocessing import Process, Pipe
+from multiprocessing.connection import PipeConnection
 from time import time, sleep
-from  os import path, remove
+from  os import remove
+from os.path import dirname, abspath
 
-def request_hourlydata(line:str, con):
-    line = [float(line[0]),float(line[1]),int(line[2]),int(line[3])]
+def request_hourlydata(coord:str, con:PipeConnection) -> None:
+    line:list = [float(coord.split(",")[0]),float(coord.split(",")[1]),int(coord.split(",")[2]),int(coord.split(",")[3])]
+    
+    file:str = "%s\\data\\hourlydata(%.6f,%.6f)[%i,%i].csv"%(dirname(abspath(__file__)),line[0],line[1],line[2],line[3])
     try:
-        directory = path.dirname(path.abspath(__file__))
-        f = open("%s\\data\\hourlydata(%f,%f)[%i,%i].csv"%(directory,line[0],line[1],line[2],line[3]), "x+b")
+        f = open(file, "xb")
     except FileExistsError as err:
         print(err)
-    else:
-        f.write(request("GET","https://re.jrc.ec.europa.eu/api/v5_2/seriescalc?lat=%f&lon=%f&startyear=%i&endyear=%i" %(line[0],line[1],line[2],line[3]),preload_content=False,timeout=None).data)
-        f.close()
-        if (open("%s\\data\\hourlydata(%f,%f)[%i,%i].csv"%(directory,line[0],line[1],line[2],line[3]), "r").readline().rsplit(":")[0]!="Latitude (decimal degrees)"):
-            remove("%s\\data\\hourlydata(%f,%f)[%i,%i].csv"%(directory,line[0],line[1],line[2],line[3]))
-            con.send(','.join(str(el) for el in line))
-
-
-
-def main(file:str):
-
-        start_time = time()
-
+    except Exception as err:
+        print(err)
+        con.send(coord)
+    else: 
         try:
-            inputs = open(file, "r")
-
-        except FileNotFoundError as err:
+            f.write(request("GET","https://re.jrc.ec.europa.eu/api/v5_2/seriescalc?lat=%.6f&lon=%.6f&startyear=%i&endyear=%i" %(line[0],line[1],line[2],line[3]),preload_content=False,timeout=None).data)
+        except Exception as err:
+            f.close()
+            remove(file)
             print(err)
-
+            con.send(coord)
         else:
-            lines = inputs.readlines()
-            inputs.close()
-            parent_cons, child_cons = zip(*map(lambda _: Pipe(False), range(len(lines)))) 
-            processes = list(map(lambda l, c: Process(target=request_hourlydata, args=[l.split(","),c]), lines, child_cons))
-            
-            total_request_delay = 0.03334*len(processes)
-            for process in processes:
-                process.start()
-                sleep(0.03334)
-            print("  Sleep duration:", total_request_delay)
+            f.close()
+            with open(file, "r") as f:
+                b:bool = f.readline().startswith("Latitude (decimal degrees):")
+            if (not(b)):
+                remove(file)
+                con.send(coord)
+    con.close()
 
-            print("   Until joining:", time()-start_time)
-            joining_start = time()
-            for process in processes:
-                process.join()
-                process.close() 
-            print("Joining duration:", time()-joining_start)
-            
-            retry_dir = "%s\\retry.dat"%(path.dirname(path.abspath(__file__)))
-            retry = open(retry_dir, "w")
-            retry.truncate(0)
-            retry.writelines(list(map(lambda c: "%s\n"%(c.recv()),filter(lambda p: p.poll(),parent_cons))))
-            retry.close()
-            retry = open(retry_dir,"r")
-            if (len(retry.readlines())>0):
-                main(retry_dir)
-            retry.close()
-            total_request_delay += time()-joining_start
 
-        print("total request delay: %f\nexecution time: %f" %(total_request_delay, time()-start_time))
+def main(file:str) -> None:
+    start_time = time()
+
+    with open("%s\\%s"%(dirname(abspath(__file__)), file), "r") as inputs:
+        lines:list[str] = inputs.readlines()
+
+    parent_cons:map[PipeConnection]
+    child_cons:map[PipeConnection]
+    parent_cons, child_cons = zip(*map(lambda _: Pipe(False), range(len(lines))))
+    processes:list[Process] = list(map(lambda line, con: Process(target=request_hourlydata, args=[line,con]), lines, child_cons))
+    
+    for process in processes:
+        process.start()
+        sleep(0.04)
+    print("  Sleep duration:", 0.04*len(processes))
+
+    for process in processes:
+        process.join()
+        process.close()
+    print("Request duration:", time()-start_time)
+    
+    parent_recvs:list[str] = [con.recv() for con in parent_cons if con.poll()]
+    if (parent_recvs):
+        with open("%s\\retry.dat"%(dirname(abspath(__file__))), "w") as retry:
+            retry.writelines(parent_recvs)
+        print("Retrying %i coordinates..."%(len(parent_recvs)))
+        main("retry.dat")
+
+    print("execution time: %f" %(time()-start_time))
             
 if __name__ == '__main__':
-    main("SP.dat")
+    main("inputs2.dat")
